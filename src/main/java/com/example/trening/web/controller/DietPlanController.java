335b8debb665f3a4f8dbcc31c08d4c8d647d8814
Bojan Dimitrovski <bokibojann@gmail.com>
2023-07-06 15:17:16 +0200

    Third commit

--- src/main/java/com/example/trening/web/controller/DietPlanController.java
+++ src/main/java/com/example/trening/web/controller/DietPlanController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/DietPlans")
 public class DietPlanController {
 
     private final DietPlanService dietPlanService;
